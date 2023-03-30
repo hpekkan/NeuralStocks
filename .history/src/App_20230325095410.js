bb89c9913aeb0a6c57af057167e7a1fac83f0f60
hpekkan <hpekkan@etu.edu.tr>
2023-03-30 11:29:03 +0300

    chore: add and remove files in project directory
    refactor(App.js): remove logo and default text, and link to React website from header

--- .history/src/App_20230325095410.js
+++ .history/src/App_20230325095410.js
@@ -1,9 +1,11 @@
-
+import logo from './logo.svg';
 import './App.css';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        
       </header>
     </div>
   );
--- .history/src/App_20230325095804.js
+++ .history/src/App_20230325095804.js
@@ -1,9 +1,11 @@
 
 import './App.css';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        
       </header>
     </div>
   );
